b540e1371460589e701869f682bc6bdf1c3f1b30
Numero7 <hugo@rhoban.com>
2018-12-19 00:08:14 +0100

    pas de petit profit

--- code_TH/arrondi_discutable.cpp
+++ code_TH/arrondi_discutable.cpp
@@ -7,7 +7,12 @@ using namespace std;
 
 int main() {
     double du = 1.499999999999999;
-    cout.precision(20);
+    cout.precision(30);
     cout << "Vous devez " << du << "euros" << endl;
     cout << "Arrondissons à leuro le plus proche i.e. à " << arrondi_euro_voisin(du) << "euros" << endl;
+
+    du = 1.999999999999999;
+    cout << "Vous devez " << du << "euros" << endl;
+    cout << "Arrondissons à leuro inférieur i.e. à " << arrondi_euro_inf(du) << "euros" << endl;
+
 }
